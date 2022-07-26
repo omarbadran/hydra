5920afdd674dd2818717e377664cc1842429af90
Omar Badran <engineer.o.badran@gmail.com>
2022-07-26 22:56:54 +0300

    Create & fetch a document

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,88 @@
-console.log('hello world');
+import Hyperbee from 'hyperbee';
+import CBOR from 'cbor';
+import { ulid } from 'ulid';
+
+type Document = {
+	[index: string]: any;
+};
+
+type Indexes = {
+	[index: string]: Hyperbee;
+};
+
+const Encoding = {
+	encode: CBOR.encode,
+	decode: CBOR.decode
+};
+
+export default class Hydra {
+	documents: Hyperbee;
+	indexes: Indexes;
+
+	/**
+	 * Create a database.
+	 *
+	 * @param core - a hypercore instance to store the documents
+	 */
+	constructor(core: any) {
+		this.documents = new Hyperbee(core, {
+			keyEncoding: Encoding,
+			valueEncoding: Encoding
+		});
+
+		this.indexes = {};
+	}
+
+	/**
+	 * Make sure the the feed is ready.
+	 *
+	 * @returns a boolean promise.
+	 * @public
+	 */
+	async ready(): Promise<boolean> {
+		await this.documents.ready();
+
+		return true;
+	}
+
+	/**
+	 * Create a new document.
+	 *
+	 * @param document - optional values with which to initialize the entity.
+	 * @returns The id of new document
+	 * @public
+	 */
+	async create(document: Document): Promise<string> {
+		if (!document?.id) {
+			document.id = ulid();
+		}
+
+		let insert = await this.documents.put(document.id, document);
+
+		return document.id;
+	}
+
+	/**
+	 * Get document by ID.
+	 *
+	 * @param id - the id of the document you seek.
+	 * @returns the matching document
+	 * @public
+	 */
+	async fetch(id: string): Promise<Document> {
+		let record = await this.documents.get(id);
+
+		return record.value;
+	}
+
+	// update(id: string, document: object): Promise<boolean> {}
+	// fetch(id: string): Promise<object | null> {}
+	// delete(id: string): Promise<boolean> {}
+	// all(): Array<object> {}
+	// search(query: object): Array<object> {}
+	// loadIndex(field: string, feed?: any): Promise<boolean> {}
+	// buildIndex(field: string, exclude: Array<string>): Promise<boolean> {}
+	// _indexDocument(doc: object): Promise<boolean> {}
+	// _deIndexDocument(id: string): Promise<boolean> {}
+	// _docFields(doc: object): Array<string> {}
+}
--- src/tests/all.test.ts
+++ src/tests/all.test.ts
@@ -1,5 +1,24 @@
 import test from 'ava';
+import Hydra from '../index';
+// @ts-ignore
+import ram from 'random-access-memory';
+// @ts-ignore
+import Hypercore from 'hypercore';
 
-test('first test', async (t) => {
-	t.assert(true);
+let core = () => {
+	return new Hypercore(ram);
+};
+
+test('Create & fetch a document', async (t) => {
+	const db = new Hydra(core());
+
+	await db.ready();
+
+	let id = await db.create({
+		name: 'omar'
+	});
+
+	let document = await db.fetch(id);
+
+	t.assert(document.name === 'omar');
 });
